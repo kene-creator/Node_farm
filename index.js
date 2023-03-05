78474a5654368f3d68e376d2b1c10537f9aaa591
Kene <kayyungx@gmail.com>
2023-03-05 12:00:49 +0100

    create templates

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  //Overview
   if (pathName === "/overview" || pathName === "/") {
     res.end("This is the overview");
+
+    //Product
   } else if (pathName === "/product") {
     res.end("This is the products");
+
+    //Api
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+
+    //Not found
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<h1>page not found</h1>");
   }
-
-  //   res.end("Hello world");
 });
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening on port 8000");
